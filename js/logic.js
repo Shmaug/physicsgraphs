4736ce67ba53a7104a5883f960577391ee49f551
Trevor Hedstrom <trevorhedstrom@gmail.com>
2015-02-15 09:05:00 -0800

    sleek transitions

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+window.loadLogic=function(slide){
     $("h2").each(function(i){
         $(this).click(function(){
             var h=36;
@@ -8,14 +8,10 @@ $(document).ready(function(){
             $(this).parent().parent().animate({height:h+"px"},500);
         });
     });
-    $(".container").each(function(i){
-        var c = document.createElement("canvas");
-        c.width=$(this).width();
-        c.height=$(this).height();
-        this.appendChild(c);
-        $(this).attr("data-ready","yes");
-    });
-    var val2=$(".content").css("margin-top");
-    $(".content").css("margin-top", "-100%");
-    $(".content").animate({"margin-top":val2},1500);
-});
+    if (slide){
+        var val2=$(".content").css("margin-top");
+        $(".content").css("margin-top", "-100%");
+        $(".content").animate({"margin-top":val2},1500);
+    }
+}
+$(document).ready(function(){loadLogic(true);});
